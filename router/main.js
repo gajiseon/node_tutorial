1c5901cdf855a05bdf03bd65114f9cb8cc6670c3
gajisun <gajisun0701@gmail.com>
2020-07-07 16:01:44 +0900

    [+]create project

---
+++ router/main.js
@@ -0,0 +1,13 @@
+module.exports = function(app, fs)
+{
+    app.get('/', function(req, res) {
+        res.render('index', {
+            title: "MY HOMEPAGE",
+            length: 5
+        });
+    });
+
+    app.get('/about', function(req,res) {
+        res.render('about.html')
+    });
+}
